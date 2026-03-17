c0bd15dd20ac40662cc669456a8bb6c1fbacf69e
srijeeta777 <sd9346@srmist.edu.in>
2026-03-17 14:09:49 +0530

    UC5

---
+++ UC5_HelloApp.java
@@ -0,0 +1,18 @@
+public class UC5_HelloApp {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            System.out.print("Hello ");
+            boolean first = true;
+            for (String name : args) {
+                if (!first) {
+                    System.out.print(", ");
+                }
+                System.out.print(name);
+                first = false;
+            }
+            System.out.println("!");
+        }
+    }
+}
